fbe7513e0bf248ce242c470b56a17350c961a25f
Pál Dániel <pal.daniel.79@gmail.com>
2022-11-20 19:41:44 +0100

    input.sh created

---
+++ shellFiles/input.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "Hello $USER"
+
+echo -n "Adja meg a vezetéknevét: "
+read vnev
+
+echo -n "Adja meg keresztnevét: "
+read knev
+
+echo "Köszönöm kedves $vnev $knev. Viszontlátásra!"
+
+exit 0
